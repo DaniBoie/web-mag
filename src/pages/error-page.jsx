5b6719d5aeccda560a60e9c68c46664dbfb0dbb9
Daniel Ayala <dannyboy3385@gmail.com>
2023-06-29 23:20:26 -0700

    style changes

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -6,9 +6,11 @@ export default function ErrorPage() {
 
   return (
     <div class="coming-soon">
-      <img src="../assets/Soli Logo.png" alt="Soli Collective Logo" />
-      <b>COMING SOON</b>
+      {/* <img src="https://raw.githubusercontent.com/DaniBoie/web-mag/main/src/assets/Soli%20Logo.png" alt="Soli Collective Logo" /> */}
+      <div class="logo"></div>
       <div class="blob"><></></div>
+      <b class="text">COMING SOON</b>
+
     </div>
   );
 }
