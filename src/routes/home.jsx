344dcf0c84bfa8f59ba9cdcc9d9d023b42f9bb84
Daniel Ayala <dannyboy3385@gmail.com>
2023-06-30 01:10:17 -0700

    altering for new page

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -4,13 +4,12 @@ export default function Home() {
 
 
   return (
-    <>
-      {/* <div class="coming-soon">
-        <b>COMING SOON</b>
-
-
-      </div> */}
+    <div class="coming-soon">
+      {/* <img src="https://raw.githubusercontent.com/DaniBoie/web-mag/main/src/assets/Soli%20Logo.png" alt="Soli Collective Logo" /> */}
+      <div class="logo"></div>
       <div class="blob"><></></div>
-    </>
+      <b class="text">COMING SOON</b>
+
+    </div>
   );
 }
