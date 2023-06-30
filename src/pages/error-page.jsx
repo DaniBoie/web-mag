f3713b9bd3e7481dab65ebd4e11a273ca86b816e
Daniel Ayala <dannyboy3385@gmail.com>
2023-06-29 22:41:12 -0700

    soli logo

--- src/pages/error-page.jsx
+++ src/pages/error-page.jsx
@@ -6,6 +6,7 @@ export default function ErrorPage() {
 
   return (
     <div class="coming-soon">
+      <img src="../assets/Soli Logo.png" alt="Soli Collective Logo" />
       <b>COMING SOON</b>
       <div class="blob"><></></div>
     </div>
