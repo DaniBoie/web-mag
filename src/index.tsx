4e0d8fbfd341516f5125ea2eab3a62fc53cd5fba
Eddie-OYO <eduardoorozco2.eo@gmail.com>
2024-08-04 21:33:14 -0700

    sound triggered by button

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import ErrorPage from './pages/error-page';
 import Home from './routes/home';
 import Game from './routes/game';
-import Tone from './routes/tone';
+import Toner from './routes/tone';
 // import { Canvas } from './routes/breakoutCanvas';
 
 const router = createBrowserRouter([
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
   },
   {
     path: 'tone',
-    element: <Tone />
+    element: <Toner />
   }
 ])
 
--- src/routes/tone.tsx
+++ src/routes/tone.tsx
@@ -1,17 +1,27 @@
-import Game from "./game";
+import *  as Tone from "tone";
 
-export default function Tone() {
+export default function Toner() {
 
     // JS HERE 
-        // WRITE CODE HERE
-    // JS HERE
+    const playBTN = document.getElementById("play-btn");
+    const synth = new Tone.Synth().toDestination() ;
 
+    playBTN?.addEventListener("click", async () => {
+        if(Tone.getContext().state !== "running"){
+            await Tone.start();
+        }
+        synth.triggerAttackRelease("A5", "8n");
+    });
+
+    // JS HERE
+console.log(Tone.context.state);
     // HTML HERE
     return (
         <>
             <h1 style={{color: "blue"}}>
                 Tone JS
             </h1>
+            <button id="play-btn">Play</button>
         </>
     )
     // HTML HERE
